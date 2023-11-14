bf05bcbdb5ce1dbf1a1e961b21d2c52d63dafd5b
unknown <ijv85378@kwansei.ac.jp>
2023-11-14 14:59:00 +0900

    koment

--- ex2.py
+++ ex2.py
@@ -1,5 +1,17 @@
 #coding:utf-8
+import json
 
-# ファイルを開く
 
+with open("catalog.json", "r") as file:
+    data = json.load(file)
     
+price = [item["price"] for item in data if item["name"] == "jacket"]
+
+count = len(price)
+
+max=max(price)
+min=min(price)
+
+print(f'個数: {count}')
+print(f'最高価格: {max}')
+print(f'最低価格: {min}')
