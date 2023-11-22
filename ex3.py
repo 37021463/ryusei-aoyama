0ebc2b2d9cc1d560a218f05a709841a75e0a6bf0
unknown <ijv85378@kwansei.ac.jp>
2023-11-22 15:29:23 +0900

    koment

--- ex3.py
+++ ex3.py
@@ -1,10 +1,16 @@
 #coding:utf-8
-import json
-
-with open("catalog.json") as f:
-    j = json.load(f)
-
-x = next((item["price"] for x in j if x["name"] == "jacket"),None)
-
-coumt = len(x)
-max = 
+import zipfile
+a=0
+total=0
+with zipfile.ZipFile('sample.zip',"r") as zf:
+    name = zf.namelist()
+    for i in name:
+        if i.endswith("_kgu.txt"):
+            with zf.open(i,"r") as f:
+                number = f.read()
+                
+                if a%2 == 1:
+                    total += int(number)
+                a +=1
+print(total)
+    
